6f6bd7710cc9a54a168348b354424f423661ce9a
Atom Scott <atom.james.scott@gmail.com>
2020-02-04 11:31:26 +0900

    base api framework

--- backend/api_dev/views.py
+++ backend/api_dev/views.py
@@ -1,8 +1,25 @@
+import requests, sys, os
+import acasearch
+
 from backend.api_dev.models import Article
 from backend.api_dev.serializers import ArticleSerializer
 from rest_framework import viewsets
+from django.http import HttpResponse
 
 # Create your views here.
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+
+def index(request):
+    query = request.GET.get('q', False)
+    if not query:
+        return HttpResponse('q parameter is empty')  
+
+    res = acasearch.arxiv_search(query, n=1)
+    prophy_id = acasearch.get_prophy_id(os.path.basename(res.get('id')))
+
+    if not prophy_id:
+        return HttpResponse('Paper not found')
+
+    return HttpResponse(acasearch.get_citation_tree(prophy_id), depth=3)
--- backend/urls.py
+++ backend/urls.py
@@ -5,11 +5,12 @@ from backend.api_dev import views
 
 router = routers.DefaultRouter()
 
-router.register(r'api_dev', views.ArticleViewSet)
+router.register(r'', views.ArticleViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api_dev/', include(router.urls)),
+    path('', views.index),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
